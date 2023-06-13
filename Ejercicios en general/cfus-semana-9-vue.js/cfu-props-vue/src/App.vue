4e67d2b488d27f0d28c3aefc18a9d5d000683f56
Emilio <blim_27@outlook.es>
2023-06-13 09:47:59 +0200

    cambios carpetas

---
+++ Ejercicios en general/cfus-semana-9-vue.js/cfu-props-vue/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Button from "./components/Button.vue";
+</script>
+
+<template>
+  <div id="app">
+    <Button name="Warning" bgColor="blue" />
+    <Button name="Danger" bgColor="red" />
+  </div>
+</template>
---
+++ Ejercicios en general/cfus-semana-9-vue.js/cfu-props-vue/src/components/Button.vue
@@ -0,0 +1,9 @@
+<script setup>
+const props = defineProps({
+  name: String,
+  bgColor: String,
+});
+</script>
+<template>
+    <button class="btn" :style="{ backgroundColor: bgColor }">{{ name }}</button>
+  </template>
